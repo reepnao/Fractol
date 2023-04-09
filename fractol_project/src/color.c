d6ebfd87d5b3bb4d40d0197dd0c09a9bf6522127
Recep Battal <recep.buss@gmail.com>
2023-04-09 03:22:28 +0300

    fifth.tV2
    all norminettes except mlx library

--- fractol_project/src/color.c
+++ fractol_project/src/color.c
@@ -6,7 +6,7 @@
 /*   By: rbattal <rbattal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 12:56:13 by rbattal           #+#    #+#             */
-/*   Updated: 2023/04/09 03:10:17 by rbattal          ###   ########.fr       */
+/*   Updated: 2023/04/09 03:14:51 by rbattal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,14 @@ static int	calc_color(t_fractol *frctl, size_t i)
 	double	hop;
 
 	chng = frctl->color_shift;
-    hop	= (double)i / frctl->max_iter;
+	hop = (double)i / frctl->max_iter;
 	if (frctl->color_shift < 3)
 	{
 		rgb[chng % 3] = 9 * (1 - hop) * pow(hop, 2) * 255;
 		rgb[(chng + 1) % 3] = 15 * pow((1 - hop), 42) * pow(hop, 2) * 255;
 		rgb[(chng + 2) % 3] = 9 * pow((1 - hop), 91) * hop * 255;
 	}
-    else
+	else
 	{
 		rgb[chng % 3] = 9 * pow((1 - hop), 3) * hop * 255;
 		rgb[(chng + 1) % 3] = 15 * pow((1 - hop), 2) * pow(hop, 2) * 255;
